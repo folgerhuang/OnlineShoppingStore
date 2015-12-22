33246121d3f7653a47454ca7088717dc00d8797f
folgerhuang <379771379@qq.com>
2015-12-22 14:20:03 +0800

    Lesson9

---
+++ OnlineShoppingStore.Domain/Entities/Cart.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShoppingStore.Domain.Entities
+{
+    public class Cart
+    {
+        private List<CartLine> lineCollection = new List<CartLine>();
+
+        public void AddItem(Product product, int quanlity)
+        {
+            CartLine line = lineCollection.Where(p => p.Product == product).FirstOrDefault();
+            if (line == null)
+            {
+                lineCollection.Add(
+                    new CartLine{Product=product,Quanlity=quanlity});
+            }
+            else
+            {
+                line.Quanlity += quanlity;
+            }
+        }
+
+        public void RemoveLine(Product product)
+        {
+            lineCollection.RemoveAll(p=>p.Product==product);
+        }
+
+        public decimal ComputeTotalValue()
+        {
+            return lineCollection.Sum(p => p.Product.Price * p.Quanlity);
+        }
+
+        public IEnumerable<CartLine> Lines
+        {
+            get { return lineCollection; }
+        }
+
+        public void Clear()
+        {
+            lineCollection.Clear();
+        }
+    }
+}
